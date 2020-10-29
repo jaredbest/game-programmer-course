1286229ce216ae2bf81d0aad0cbfbb6688f35e01
jaredbest <me@jaredbest.com>
2020-10-29 16:00:40 +0100

    Finished Lesson 7B

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -50,6 +50,7 @@ public class Player : MonoBehaviour
         {
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, _jumpVelocity);
             _jumpsRemaining--;
+            Debug.Log($"Jumps remaining {_jumpsRemaining}");
             _fallTimer = 0;
             _jumpTimer = 0;
         }
@@ -57,10 +58,10 @@ public class Player : MonoBehaviour
         {
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, _jumpVelocity);
             _fallTimer = 0;
-            _jumpTimer += Time.deltaTime;
         }
+        _jumpTimer += Time.deltaTime;
 
-        if (isGrounded)
+        if (isGrounded && _fallTimer > 0)
         {
             _fallTimer = 0;
             _jumpsRemaining = _maxJumps;
