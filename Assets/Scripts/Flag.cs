252fc1403adb49f18f6da472218e6f3a9dda2487
jaredbest <me@jaredbest.com>
2020-10-29 15:08:35 +0100

    Finished Lesson 6D

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Flag : MonoBehaviour
+{
+    [SerializeField] string _sceneName;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player == null)
+            return;
+
+        var animator = GetComponent<Animator>();
+        animator.SetTrigger("Raise");
+
+        SceneManager.LoadScene(_sceneName);
+    }
+}
