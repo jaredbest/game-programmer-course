2edc08b88f27cfa11894b97dcbb400ddc5f34fdd
jaredbest <me@jaredbest.com>
2020-10-29 13:13:08 +0100

    Finished Lesson 6A

---
+++ Assets/Scripts/HorizontalCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalCamera : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(_target.position.x, transform.position.y, transform.position.z);
+    }
+}
