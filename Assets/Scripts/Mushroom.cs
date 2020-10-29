c942b0729c49682582b2dc92f7e683a79e6d47a6
jaredbest <me@jaredbest.com>
2020-10-29 16:09:37 +0100

    Finished Lesson 7C

---
+++ Assets/Scripts/Mushroom.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Mushroom : MonoBehaviour
+{
+
+    [SerializeField] float _bounceVelocity = 10;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var player = collision.collider.GetComponent<Player>();
+
+        if (player != null)
+        {
+            var rigidbody2D = player.GetComponent<Rigidbody2D>();
+            if (rigidbody2D != null)
+            {
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, _bounceVelocity);
+            }
+        }
+    }
+}
