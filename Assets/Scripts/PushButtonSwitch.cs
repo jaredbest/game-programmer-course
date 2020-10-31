4a8b2ed0c671cacc83fd6fcaabd8bd4029cfc0aa
jaredbest <me@jaredbest.com>
2020-10-31 10:23:26 +0100

    Complete Lesson 8B

---
+++ Assets/Scripts/PushButtonSwitch.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PushButtonSwitch : MonoBehaviour
+{
+    [SerializeField] Sprite _pressedSprite;
+    [SerializeField] UnityEvent _onPressed;
+    [SerializeField] UnityEvent _onReleased;
+    SpriteRenderer _spriteRenderer;
+    Sprite _releasedSprite;
+
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _releasedSprite = _spriteRenderer.sprite;
+        BecomeReleased();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player == null)
+            return;
+
+        BecomePressed();
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player == null)
+            return;
+
+        BecomeReleased();
+    }
+
+    void BecomePressed()
+    {
+        _spriteRenderer.sprite = _pressedSprite;
+        _onPressed?.Invoke();
+    }
+
+    void BecomeReleased()
+    {
+        _spriteRenderer.sprite = _releasedSprite;
+        _onReleased?.Invoke();
+    }
+}
