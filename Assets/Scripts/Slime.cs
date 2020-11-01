d077c02ff6945fd79bfd05e8379fe0d51c05a0eb
jaredbest <me@jaredbest.com>
2020-11-01 18:43:16 +0100

    Completed Lesson 9A

---
+++ Assets/Scripts/Slime.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : MonoBehaviour
+{
+    Rigidbody2D _rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody2D.velocity = Vector2.left;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        var player = collision.collider.GetComponent<Player>();
+        if (player == null)
+            return;
+
+        player.ResetToStart();
+    }
+}
