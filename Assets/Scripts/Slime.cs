56edb2750ee6b8f9e2e026dec2514570362321a7
jaredbest <me@jaredbest.com>
2020-11-02 19:27:33 +0100

    Completed Lesson 9C

--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    [SerializeField] Transform _leftSensor;
+    [SerializeField] Transform _rightSensor;
+
     Rigidbody2D _rigidbody2D;
+    float _direction = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,29 @@ public class Slime : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _rigidbody2D.velocity = Vector2.left;
+        _rigidbody2D.velocity = new Vector2(_direction, _rigidbody2D.velocity.y);
+        
+        if(_direction < 0)
+        {
+            Debug.DrawRay(_leftSensor.position, Vector2.down * 0.1f, Color.red);
+            var result = Physics2D.Raycast(_leftSensor.position, Vector2.down, 0.1f);
+            if (result.collider == null)
+                TurnAround();
+        }
+        else
+        {
+            Debug.DrawRay(_rightSensor.position, Vector2.down * 0.1f, Color.red);
+            var result = Physics2D.Raycast(_rightSensor.position, Vector2.down, 0.1f);
+            if (result.collider == null)
+                TurnAround();
+        }
+    }
+
+    void TurnAround()
+    {
+        _direction *= -1;
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.flipX = _direction > 0;
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
