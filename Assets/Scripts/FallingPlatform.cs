e56fc29aee7ea0bfe58c41c2aea32cb1589d3f8b
jaredbest <me@jaredbest.com>
2020-12-14 18:42:32 +0100

    Updated version to 2020.1.17f1 and updated package Test Framework

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -7,6 +7,13 @@ public class FallingPlatform : MonoBehaviour
 {
     public bool PlayerInside;
     HashSet<Player> _playersInTrigger = new HashSet<Player>();
+    Coroutine _coroutine;
+    Vector3 _initialPosition;
+
+    void Start()
+    {
+        _initialPosition = transform.position;
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -18,7 +25,8 @@ public class FallingPlatform : MonoBehaviour
 
         PlayerInside = true;
 
-        StartCoroutine(WiggleAndFall());
+        if (_playersInTrigger.Count == 1)
+            StartCoroutine(WiggleAndFall());
     }
 
     IEnumerator WiggleAndFall()
@@ -26,8 +34,28 @@ public class FallingPlatform : MonoBehaviour
         Debug.Log("Waiting to wiggle");
         yield return new WaitForSeconds(0.25f);
         Debug.Log("Wiggling");
-        yield return new WaitForSeconds(1f);
+        float wiggleTimer = 0;
+
+        while (wiggleTimer < 1f)
+        {
+            float randomX = UnityEngine.Random.Range(-0.05f, 0.05f);
+            float randomY = UnityEngine.Random.Range(-0.05f, 0.05f);
+            transform.position = _initialPosition + new Vector3(randomX, randomY);
+            float randomDelay = UnityEngine.Random.Range(0.005f, 0.01f);
+            yield return new WaitForSeconds(randomDelay);
+            wiggleTimer += randomDelay;
+        }
+
         Debug.Log("Falling");
+
+        float fallTimer = 0;
+
+        while (fallTimer < 3Ff)
+        {
+            transform.position += Vector3.down * Time.deltaTime * _fallSpeed;
+        }
+
+
         yield return new WaitForSeconds(3f);
     }
 
@@ -40,6 +68,9 @@ public class FallingPlatform : MonoBehaviour
         _playersInTrigger.Remove(player);
 
         if (_playersInTrigger.Count == 0)
+        {
             PlayerInside = false;
+            StopCoroutine(WiggleAndFall());
+        }
     }
 }
