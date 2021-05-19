7f70eb31b27662c2f4c0f7a118db56cb7d57ceab
jaredbest <me@jaredbest.com>
2021-05-19 16:38:57 +0200

    Finished Lesson 14E.

--- Assets/Scripts/KillOnEnter.cs
+++ Assets/Scripts/KillOnEnter.cs
@@ -2,9 +2,18 @@
 
 public class KillOnEnter : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         var player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            player.ResetToStart();
+        }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        var player = other.GetComponent<Player>();
         if (player != null)
         {
             player.ResetToStart();
